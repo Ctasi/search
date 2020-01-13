d3bbe3d851c794a0a45d6a4592405d2262113b6a
ctasi <tasi79810@gmail.com>
2020-01-13 17:36:56 +0800

    input登录

--- seach.py
+++ seach.py
@@ -1,5 +1,8 @@
 from PyQt5.Qt import *
+from PyQt5 import QtGui
 import sys
+import json
+import requests
 
 class login(QWidget):
 
@@ -48,13 +51,40 @@ class login(QWidget):
         btnBig.setText('最大化')
         #账号
         self.textbox = QLineEdit(self)
-        self.textbox.move(290, 140)
+        self.textbox.move(290, 160)
         self.textbox.resize(220, 40)
         self.textbox.setObjectName('userInput')
         self.textbox.setFont(QFont("Microsoft YaHei" , 18, 75))
         #标题
-        self.h1 = labelbg.setText('213213')
+        self.h1 = QLabel(self)
+        self.h1.setText('cccccccc')
+        self.h1.move(290, 80)
+        self.textbox.setFont(QFont("Microsoft YaHei" , 18, 75))
+        self.h1.setStyleSheet("color:#ffffff;font-size:50px;")
+
+        #登录按钮
+        btnLogin = QPushButton(self)
+        btnLogin.setObjectName('btnLogin')
+        btnLogin.resize(100,40)
+        btnLogin.move(290,220)
+        btnLogin.setCursor(QCursor(Qt.PointingHandCursor))
+        btnLogin.setText('登录')
+        btnLogin.clicked.connect(self.login_in)
         self.show()
+
+    def login_in(self):
+        username = self.textbox.text()
+        # password = str(self.lineEdit_passwd.text())
+        baseurl = 'http://api.yzt-tools.com/api/wtx/report/adminloginIn'
+        data = {'username': str(username)}
+        try:
+            res = requests.post(url=baseurl, data=data, timeout=3)
+        except Exception as e:
+            #QtGui.QMessageBox.critical(self, u'错误', u'用户名或密码错误')
+            return
+
+        if res.status_code == 200:
+            #QtGui.QMessageBox.critical(self, u'错误', u'用户名或密码错误')
 
     #可拖动边框窗口
     def mousePressEvent(self, event):
